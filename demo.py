2bc00068ce97e4c781d46ca284a279919ea4855d
simmuuuu <141730290+simmuuuu@users.noreply.github.com>
2023-08-08 17:13:19 +0530

    Update demo.py
    demo.py by branch 2

--- demo.py
+++ demo.py
@@ -2,3 +2,11 @@ print ("hello world")
 #this code will print python
 print ("hello world")
 #this code will print python
+print ("hello world")
+#this code will print python
+print ("hello world")
+#this code will print python
+print ("hello world")
+#this code will print python
+print ("hello world")
+#this code will print python
